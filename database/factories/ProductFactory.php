5eda3daf8f5719cf366f2878d4f3e2f6f00e78a7
myselfaashish <itsmeaashish@hotmail.com>
2019-04-13 10:40:36 +0545

    DB successfully seeded

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,6 +4,10 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Model\Product::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->word,
+        'detail' => $faker->paragraph,
+        'price' => $faker->numberBetween(1000, 6900),
+        'stock' => $faker->numberBetween(10, 90),
+        'discount' => $faker->numberBetween(10, 30)
     ];
 });
