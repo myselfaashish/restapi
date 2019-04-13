8371ff6ebf6808cbe367962efda291055d8556e0
myselfaashish <itsmeaashish@hotmail.com>
2019-04-13 13:20:33 +0545

    Resource Collection Created

--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Resources\Product;
 
-use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Http\Resources\Json\Resource;
 
-class ProductCollection extends ResourceCollection
+class ProductCollection extends Resource
 {
     /**
      * Transform the resource collection into an array.
@@ -14,6 +14,14 @@ class ProductCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'name' => $this->name,
+            'discount' => $this->discount,
+            'totalPrice' => $this->discount > 0 ? round($this->price - (($this->discount / 100) * $this->price), 2) : $this->price,
+            'ratings' => $this->reviews->count() > 0 ? round($this->reviews->sum('star') / $this->reviews->count(), 2) : 'No reviews yet',
+            'href' => [
+                'details' => route('products.show', $this->id)
+            ]
+        ];
     }
 }
